d6d6ef8a8074b2580db74239286e477b5c681d9e
ceduardo <cs.eduardo@icloud.com>
2022-02-12 12:02:51 -0300

    updates text, sizes and colors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import React from "react";
+import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div className="MainPage">
+    <div className="MainText">
+      <div className="Welcome">Welcome!</div>
+      <div>I'm Carlos, full stack software engineer!</div>
+      <div>
+        I'm passionate about building stuff for the web, both in the back as
+        well as the front end, always keeping my focus on the user experience
+        and needs.
+      </div>
+    </div>
+    <div className="ContactButton">
+      <a href="https://www.linkedin.com/in/carlos-eduardo-da-silva-420411116/">
+        Contact me!
+      </a>
+    </div>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
