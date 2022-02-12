78487ec72d53d70e851e319525ee42ea12c1b969
ceduardo <cs.eduardo@icloud.com>
2022-02-12 13:47:38 -0300

    Adds fade in

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import './index.css'
+import "./index.css";
+import FadeIn from "react-fade-in";
 
 ReactDOM.render(
   <div className="MainPage">
-    <div className="MainText">
+    <FadeIn className="MainPageFadeIn"delay={125}>
       <div className="Welcome">Welcome!</div>
-      <div>I'm Carlos, full stack software engineer!</div>
-      <div>
-        I'm passionate about building stuff for the web, both in the back as
-        well as the front end, always keeping my focus on the user experience
-        and needs.
+      <div className="MainText">
+        <div>I'm Carlos, full stack software engineer!</div>
+        <div>
+          I'm passionate about building stuff for the web, both in the back as
+          well as the front end, always keeping my focus on the user experience
+          and needs.
+        </div>
       </div>
-    </div>
-    <div className="ContactButton">
-      <a href="https://www.linkedin.com/in/carlos-eduardo-da-silva-420411116/">
-        Contact me!
-      </a>
-    </div>
+      <div className="ContactButton">
+        <a href="https://www.linkedin.com/in/carlos-eduardo-da-silva-420411116/">
+          Contact me!
+        </a>
+      </div>
+    </FadeIn>
   </div>,
   document.getElementById("root")
 );
