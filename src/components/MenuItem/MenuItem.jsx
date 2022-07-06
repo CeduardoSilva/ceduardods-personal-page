b635b33c847630d17669fe95cbb72b4a62ca08d0
ceduardo <cs.eduardo@icloud.com>
2022-07-05 22:37:16 -0300

    Updates button style

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -5,9 +5,9 @@ import "./MenuItem.css";
 const MenuItem = (props) => {
     const { label, onPress } = props;
     return (
-        <div className="MenuItem">
+        <div className="MenuItemContainer"  onClick={onPress}>
             <div className="MenuItemSquare"></div>
-            <button onClick={onPress}>{label}</button>
+            {label}
         </div>
     );
 }
